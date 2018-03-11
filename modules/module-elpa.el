2686199322c76fda15c7e5f20f563dbba738a948
Bartłomiej Świercz <bartek@rndity.com>
2018-03-11 15:51:11 +0100

    Clear old config files

--- modules/module-elpa.el
+++
@@ -1,9 +0,0 @@
-(require 'package)
-
-;; enable marmalade repository
-(add-to-list 'package-archives 
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-(package-initialize)
-
-(provide 'module-elpa)
--- modules/module-snippets.el
+++
@@ -1,7 +0,0 @@
-;yasnippet
-(require 'yasnippet)
-
-(yas/initialize)
-(yas/load-directory (concat local-yasnippet-dir "/snippets"))
-
-(provide 'module-snippets)
